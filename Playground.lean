21bcb2e21c22bff68ed2f322b443ec5d3b74ca8d
Youwei Zhong <youwei.zhong@yale.edu>
2025-11-18 03:23:47 -0500

    add example

--- Playground.lean
+++ Playground.lean
@@ -1,1 +1,6 @@
 import Playground.Basic
+theorem and_commutative (p q : Prop) : p ∧ q → q ∧ p :=
+  fun hpq : p ∧ q =>
+  have hp : p := And.left hpq
+  have hq : q := And.right hpq
+  show q ∧ p from And.intro hq hp
